890552d1ce673d65146f21f8ccf973fa36d8b8d6
aharon weber <a.w770770@gmail.com>
2026-05-31 09:31:54 +0300

    Add Circle.get_area

--- circle.py
+++ circle.py
@@ -6,3 +6,6 @@ class Circle(Shape):
     def __init__(self, shape_id, radius):
         super().__init__(shape_id, "circle")
         self.radius = radius
+
+    def get_area(self):
+        return math.pi * self.radius ** 2
