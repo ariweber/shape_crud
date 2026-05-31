2f1ce2242ba5ef566b887f72412fdbd6892ebb73
aharon weber <a.w770770@gmail.com>
2026-05-31 09:33:53 +0300

    Add read_int validation helper

--- helpers.py
+++ helpers.py
@@ -9,3 +9,11 @@ def read_positive_float(prompt):
             print("Value must be greater than 0.")
             continue
         return value
+
+
+def read_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Please enter a whole number.")
