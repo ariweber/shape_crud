c560c31ab6a14a6d6d7b5533db9d47bbb27745c9
aharon weber <a.w770770@gmail.com>
2026-05-31 09:33:44 +0300

    Add read_positive_float validation helper

---
+++ helpers.py
@@ -0,0 +1,11 @@
+def read_positive_float(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+        except ValueError:
+            print("Please enter a number.")
+            continue
+        if value <= 0:
+            print("Value must be greater than 0.")
+            continue
+        return value
