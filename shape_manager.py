0bc6f8c88d5779c64463e4d26b109751e3f36971
aharon weber <a.w770770@gmail.com>
2026-05-31 09:34:05 +0300

    Add shape_manager module setup

---
+++ shape_manager.py
@@ -0,0 +1,10 @@
+import json
+from log import create_log
+from circle import Circle
+from rectangle import Rectangle
+from square import Square
+
+
+logger = create_log("shape.log")
+
+JSON_FILE = "shapes.json"
