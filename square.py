1a6f8a54dd3eb77e0bc3ee68ea00d83763314f6f
aharon weber <a.w770770@gmail.com>
2026-05-31 09:33:06 +0300

    Add Square.get_area

--- square.py
+++ square.py
@@ -5,3 +5,6 @@ class Square(Shape):
     def __init__(self, shape_id, side):
         super().__init__(shape_id, "square")
         self.side = side
+
+    def get_area(self):
+        return self.side * self.side
